1356639a448c8656c661ee93697e89fc2905560c
Haleigh Rzonca <hgielah@gmail.com>
2023-01-09 12:39:09 -0800

    remove params on putdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const getDb = async () => {
 };
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async () => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
