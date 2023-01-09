80097b035a3309814312222d0144d2b6cceec392
Haleigh Rzonca <hgielah@gmail.com>
2023-01-09 12:44:26 -0800

    fixing webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,16 +24,10 @@ module.exports = () => {
       }),
      
       // Injects our custom service worker
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'text-editor',
+        name: 'txtedit-pwa',
         short_name: 'JATE',
         description: 'Just Another Text Editor!',
         background_color: '#225ca3',
@@ -59,7 +53,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
+        
           use: {
             loader: 'babel-loader',
             options: {
