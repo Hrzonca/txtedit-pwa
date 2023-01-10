ef8ae661ca308c8b8cd3fbb33435f041b88b9615
Haleigh Rzonca <hgielah@gmail.com>
2023-01-09 19:45:27 -0800

    trying to get the header to appear

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,13 +27,13 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 //Input??
-export const getDb = async () => {
+export const getDb = async (content) => {
   console.log('GET implemented');
   const jateDb = await openDB('jate', 1);
   console.log('opened db successfully');
   const text = jateDb.transaction('jate', 'readonly');
   const store = text.objectStore('jate');
-  const request = store.get(1);
+  const request = store.getAll(content);
   const result = await request;
   console.log('saved to database', result.value);
 };
